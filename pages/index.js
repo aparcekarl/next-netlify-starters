cd164b387d829d417de331c18b82a1778681d2c9
Karl Aparece <aparecekarl@yahoo.com>
2023-08-06 21:23:00 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,16 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 <script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
 <script>
     Weglot.initialize({
         api_key: 'wg_574b4528a122ac4c88eaf2fbcca081b39'
     });
 </script>
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <Header title="Subdomain Takeover" />
         <p className="description">
