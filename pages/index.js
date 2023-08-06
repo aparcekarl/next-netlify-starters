1cc3df88924d1ecde977d980515480d1f9ed9349
Karl Aparece <aparecekarl@yahoo.com>
2023-08-06 21:19:42 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,15 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
 <script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>
 <script>
     Weglot.initialize({
         api_key: 'wg_574b4528a122ac4c88eaf2fbcca081b39'
     });
 </script>
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
